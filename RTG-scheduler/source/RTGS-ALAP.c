1606415b38747641c6ae8439968fddeca2bdf345
Nagesh gowda <knagesh@amd.com>
2018-05-13 22:12:18 -0700

    Mode 5 - ALAP Function fix

--- RTG-scheduler/source/RTGS-ALAP.c
+++ RTG-scheduler/source/RTGS-ALAP.c
@@ -11,21 +11,34 @@ int ALAP
 	int jobNumber,
 	int present_time,
 	int processors_available,
-	scheduledResourceNode ** processorsAllocatedList,
+	scheduledResourceNode **processorsAllocatedList,
 	scheduledResourceNode **jobScheduledQueueList
 )
 {
-	PROFILER_START(SRTG, ALAP)
-	int Pro = 0, job_release_time, processor_release_time = 0, processors_allocated = 0;
-	genericBackupNode *P_Given_list = NULL;
+	int localProcessors = 0, job_release_time, processor_release_time = 0, processors_allocated = 0;
+	genericBackupNode *processorsDistList = NULL;
 	processors_allocated = jobAttributesList[jobNumber].processor_req;
 	processor_release_time = jobAttributesList[jobNumber].deadline;
 	job_release_time = processor_release_time - jobAttributesList[jobNumber].execution_time;
-	Pro = processors_available;
+	localProcessors = processors_available;
 
-	if (Pro >= jobAttributesList[jobNumber].processor_req)
+	// fail case return
+	if (present_time >= job_release_time) {
+		jobAttributesList[jobNumber].schedule_hardware = 2;
+		jobAttributesList[jobNumber].rescheduled_execution = -1;
+		jobAttributesList[jobNumber].completion_time = -1;
+		jobAttributesList[jobNumber].scheduled_execution = -1;
+		GLOBAL_CPU_JOBS++;
+		if (GLOBAL_RTGS_DEBUG_MSG > 1) {
+			printf("Mode 4 ALAP: The Job:%d Cannot be scheduled\n", jobNumber);
+			printf("Mode 4 ALAP: Jobs REJECTED count --> %d\n", GLOBAL_CPU_JOBS);
+		}
+		return processors_available;
+	}
+
+	if (localProcessors >= jobAttributesList[jobNumber].processor_req)
 	{
-		int processorReleased = jobAttributesList[jobNumber].processor_req;
+		int processorsInUse = jobAttributesList[jobNumber].processor_req;
 		int schedule_method = RTGS_SCHEDULE_METHOD_ALAP;
 
 		jobAttributesList[jobNumber].schedule_hardware = 1;
@@ -33,28 +46,27 @@ int ALAP
 		jobAttributesList[jobNumber].scheduled_execution = job_release_time;
 		jobAttributesList[jobNumber].completion_time = jobAttributesList[jobNumber].execution_time + job_release_time;
 		GLOBAL_GPU_JOBS++;
-		if (GLOBAL_RTGS_DEBUG_MSG > 2) {
-			printf("As Lata As Possible (ALAP) -- Job-%d scheduled\n", jobNumber);
-			printf("ALAP -- Jobs ACCEPTED count --> %d\n", GLOBAL_GPU_JOBS);
+		if (GLOBAL_RTGS_DEBUG_MSG > 1) {
+			printf("Mode 4 ALAP: The Job:%d scheduled\n", jobNumber);
+			printf("Mode 4 ALAP: Jobs ACCEPTED count --> %d\n", GLOBAL_GPU_JOBS);
 		}
-		GLOBAL_preScheduleList = insert_preScheduledJob_list(GLOBAL_preScheduleList, job_release_time, processor_release_time,
-			processors_allocated, jobNumber);
-		job_queue_handler(processorReleased, job_release_time, processor_release_time,
+		GLOBAL_preScheduleList = insert_preScheduledJob_list(GLOBAL_preScheduleList, job_release_time,
+			processor_release_time, processors_allocated, jobNumber);
+		job_queue_handler(processorsInUse, job_release_time, processor_release_time,
 			schedule_method, jobNumber, jobScheduledQueueList);
-		PROFILER_STOP(SRTG, ALAP)
-			return processors_available;
+		return processors_available;
 	}
-	P_Given_list = insert_preScheduledJob_list(P_Given_list, job_release_time, processor_release_time,
-		processors_available, jobNumber);
-	scheduledResourceNode* temp = *processorsAllocatedList;
 
-	while (temp != NULL)
+	processorsDistList = insert_preScheduledJob_list(processorsDistList, job_release_time, processor_release_time, processors_available, jobNumber);
+	scheduledResourceNode *localProcessorsAllocatedList = *processorsAllocatedList;
+
+	while (localProcessorsAllocatedList != NULL)
 	{
-		if ((temp->processor_release_time + jobAttributesList[jobNumber].execution_time) > jobAttributesList[jobNumber].deadline)
+		if ((localProcessorsAllocatedList->processor_release_time + jobAttributesList[jobNumber].execution_time) > jobAttributesList[jobNumber].deadline)
 		{
 			int count = 0;
 			scheduledResourceNode*temp1 = *processorsAllocatedList;
-			genericBackupNode* temp2 = P_Given_list;
+			genericBackupNode* temp2 = processorsDistList;
 
 			while (temp2 != NULL)
 			{
@@ -67,27 +79,26 @@ int ALAP
 				}
 				count++;
 			}
-			P_Given_list = clean_list(P_Given_list);
+			processorsDistList = clean_list(processorsDistList);
 			jobAttributesList[jobNumber].schedule_hardware = 2;
 			jobAttributesList[jobNumber].rescheduled_execution = -1;
 			jobAttributesList[jobNumber].completion_time = -1;
 			jobAttributesList[jobNumber].scheduled_execution = -1;
 			GLOBAL_CPU_JOBS++;
-			if (GLOBAL_RTGS_DEBUG_MSG > 2) {
-				printf("As Lata As Possible (ALAP) -- Job-%d Cannot be scheduled\n", jobNumber);
-				printf("ALAP -- Jobs REJECTED count --> %d\n", GLOBAL_CPU_JOBS);
+			if (GLOBAL_RTGS_DEBUG_MSG > 1) {
+				printf("Mode 4 ALAP: The Job:%d Cannot be scheduled\n", jobNumber);
+				printf("Mode 4 ALAP: Jobs REJECTED count --> %d\n", GLOBAL_CPU_JOBS);
 			}
-			PROFILER_STOP(SRTG, ALAP)
-			return processors_available;
+			break;
 		}
 		else
 		{
-			Pro = Pro + temp->processors_allocated;
-			if (Pro >= jobAttributesList[jobNumber].processor_req)
+			localProcessors = localProcessors + localProcessorsAllocatedList->processors_allocated;
+			if (localProcessors >= jobAttributesList[jobNumber].processor_req)
 			{
-				temp->job_release_time = job_release_time;
-				P_Given_list = clean_list(P_Given_list);
-				int processorReleased = jobAttributesList[jobNumber].processor_req;
+				localProcessorsAllocatedList->job_release_time = job_release_time;
+				processorsDistList = clean_list(processorsDistList);
+				int processorsInUse = jobAttributesList[jobNumber].processor_req;
 				int schedule_method = RTGS_SCHEDULE_METHOD_ALAP;
 
 				jobAttributesList[jobNumber].schedule_hardware = 1;
@@ -95,47 +106,23 @@ int ALAP
 				jobAttributesList[jobNumber].scheduled_execution = job_release_time;
 				jobAttributesList[jobNumber].completion_time = jobAttributesList[jobNumber].execution_time + job_release_time;
 				GLOBAL_GPU_JOBS++;
-				if (GLOBAL_RTGS_DEBUG_MSG > 2) {
-					printf("As Lata As Possible (ALAP) -- Job-%d scheduled\n", jobNumber);
-					printf("ALAP -- Jobs ACCEPTED count --> %d\n", GLOBAL_GPU_JOBS);
+				if (GLOBAL_RTGS_DEBUG_MSG > 1) {
+					printf("Mode 4 ALAP: The Job:%d scheduled\n", jobNumber);
+					printf("Mode 4 ALAP: Jobs ACCEPTED count --> %d\n", GLOBAL_GPU_JOBS);
 				}
 				GLOBAL_preScheduleList = insert_preScheduledJob_list(GLOBAL_preScheduleList, job_release_time, processor_release_time,
 					processors_allocated, jobNumber);
-				job_queue_handler(processorReleased, job_release_time, processor_release_time,
+				job_queue_handler(processorsInUse, job_release_time, processor_release_time,
 					schedule_method, jobNumber, jobScheduledQueueList);
-				PROFILER_STOP(SRTG, ALAP)
-				return processors_available;
+				break;
 			}
-			else if (Pro < jobAttributesList[jobNumber].processor_req)
+			else
 			{
-				P_Given_list = insert_preScheduledJob_list(P_Given_list, job_release_time, processor_release_time, temp->processors_allocated, jobNumber);
-				temp->job_release_time = job_release_time;
-				temp = temp->next;
+				processorsDistList = insert_preScheduledJob_list(processorsDistList, job_release_time, processor_release_time, localProcessorsAllocatedList->processors_allocated, localProcessorsAllocatedList->jobNumber);
+				localProcessorsAllocatedList->job_release_time = job_release_time;
 			}
 		}
+		localProcessorsAllocatedList = localProcessorsAllocatedList->next;
 	}
-	if (temp == NULL && P_Given_list != NULL)
-	{
-		int count = 0;
-		scheduledResourceNode*temp1 = *processorsAllocatedList;
-		genericBackupNode* temp2 = P_Given_list;
-		while (temp2 != NULL)
-		{
-			if (count == 0) {
-				temp2 = temp2->next;
-			}
-			else {
-				temp1->job_release_time = 0;
-				temp1 = temp1->next;
-				temp2 = temp2->next;
-			}
-			count++;
-		}
-		P_Given_list = clean_list(P_Given_list);
-		if (GLOBAL_RTGS_DEBUG_MSG > 2) {
-			printf("As Lata As Possible (ALAP) -- Job-%d Cannot be scheduled AEAP", jobNumber);
-		}
-	}
-	PROFILER_STOP(SRTG, ALAP)
 	return processors_available;
 }
