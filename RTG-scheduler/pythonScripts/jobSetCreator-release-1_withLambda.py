8a699357e64f068bf4ecb84b3a5df3db4e07edda
Kiriti <kiritigowda@gmail.com>
2018-05-31 15:09:48 -0700

    Job Set Creator with user lambda implemented

--- RTG-scheduler/pythonScripts/jobSetCreator-release-1_withLambda.py
+++ RTG-scheduler/pythonScripts/jobSetCreator-release-1_withLambda.py
@@ -12,7 +12,8 @@ numJobSet = -1
 numJobsPerSet = -1
 maxProcessors = -1
 lambdaVar = -1
-alapLimit = 10
+ALAP_limit = ''
+alapLimit = -1
 
 for opt, arg in opts:
     if opt == '-d':
@@ -28,16 +29,16 @@ for opt, arg in opts:
     elif opt == '-l':
     	lambdaVar = float(arg);
     elif opt == '-a':
-    	alapLimit = float(arg);
+		ALAP_limit = arg;
 
 if outputDirectory == '' or jobSetName == '' or numJobSet == -1 or numJobsPerSet == -1 or maxProcessors == -1 or lambdaVar == -1:
-    print('Invalid command line arguments. 		 -d [output Directory - required] ' \
-          				  						'-s [job set name - required] ' \
-          				  						'-n [number of job sets to be created - required] ' \
-          				  						'-j [number of job per set to be created - required]  ' \
-          				  						'-p [max processors available - required]' \
-          				  						'-l [lambda job arrival rate [range:0.001 to 1] - required]'\
-          				  						'-a [Schedule ALAP processor limit - optional]')
+    print('Invalid command line arguments.	-d [output Directory - required] ' \
+											'-s [job set name - required] ' \
+											'-n [number of job sets to be created - required] ' \
+											'-j [number of job per set to be created - required]  ' \
+											'-p [max processors available - required]' \
+											'-l [lambda job arrival rate [range:0.001 to 1] - required]'\
+											'-a [Schedule ALAP processor limit - optional [default: 0.75 * max processors]]')
     exit()
 
 if not os.path.exists(outputDirectory):
@@ -46,6 +47,13 @@ if not os.path.exists(outputDirectory):
 if lambdaVar <= 0:
 	print ("lambda Varible should be in the range 0.001 to 1");
 	exit();
+
+if ALAP_limit == '':
+	alapLimit = int(0.75*maxProcessors);
+else:
+	ALAP_limit.strip();
+	alapLimit = int(ALAP_limit);
+
 
 # num job sets required to be created
 for s in range(numJobSet):
@@ -64,17 +72,17 @@ for s in range(numJobSet):
 	
 		for x in range(numJobsPerSet):
 			jobNumber = x;
-			numProcessors = random.randint(1,maxProcessors);
-			if numProcessors % 2 != 0:
-				numProcessors += 1;
-			executionTime = random.randint(2,maxProcessors) + random.randint(0,random.randint(0,numProcessors));
-			if alapLimit != -1 and numProcessors >= alapLimit:
-				executionTime = executionTime + random.randint(0,numProcessors);
-			releaseDistribution = int(random.expovariate(lambdaVar))
+			numProcessors = random.randrange(2,maxProcessors,2);
+			numProcessorInverseVar = float(1)/numProcessors;
+			executionTime = random.randint(numProcessors, max( numProcessors, int(random.expovariate(numProcessorInverseVar)) ));
+			releaseDistribution = int(random.expovariate(lambdaVar));
 			releaseTime = releaseTime + releaseDistribution;
-			lastestTimeSchedulable = releaseTime + executionTime + numProcessors
-			deadLineVar = float(1)/numProcessors;
-			deadLine = lastestTimeSchedulable + int(random.expovariate(deadLineVar));
+			lastestTimeSchedulable = releaseTime + executionTime;
+			if(alapLimit < numProcessors):
+				deadlineVar = numProcessorInverseVar;
+			else:
+				deadlineVar = numProcessorInverseVar/10;
+			deadLine = lastestTimeSchedulable + int(random.expovariate(deadlineVar));
 			releaseTimeArray[x] = releaseTime
 			releaseTime = releaseTime + 1
 			f.write(str(jobNumber)+','+str(numProcessors)+','+str(executionTime)+','+str(deadLine)+','+str(lastestTimeSchedulable)+'\n')
