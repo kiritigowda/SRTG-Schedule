b724982cda354b8a2fdd4b8098f1e5f7ef252b2f
kiriti <kiritigowda@gmail.com>
2018-07-14 15:35:10 -0700

    RTGS - 1.0.0 -- Removed Not Implemented Function

--- RTG-scheduler/source/RTGS-AEAP_ALAP_improve.c
+++ RTG-scheduler/source/RTGS-AEAP_ALAP_improve.c
@@ -15,20 +15,6 @@ int AEAP_ALAP_improve
 	scheduledResourceNode **jobScheduledQueueList
 )
 {
-
-	if (GLOBAL_RTGS_DEBUG_MSG > 2) {
-		printf("AEAP_ALAP_IMPROVE: ALAP IMPROVED scheduling\n");
-		printf("AEAP_ALAP_IMPROVE: Job Release Time: %d\n", job_release_time);
-	}
-	scheduledResourceNode *temp = *jobScheduledQueueList;
-	while (temp->jobNumber != GLOBAL_preScheduleList->jobNumber && temp->next != NULL)
-		temp = temp->next;
-
-	if (temp->jobNumber == GLOBAL_preScheduleList->jobNumber && temp->processors_allocated == GLOBAL_preScheduleList->processors_allocated)
-	{
-		temp->data = temp->job_release_time = GLOBAL_preScheduleList->data = job_release_time;
-		temp->processor_release_time = GLOBAL_preScheduleList->processor_release_time = job_release_time + jobAttributesList[temp->jobNumber].execution_time;
-	}
-
+	printf("AEAP_ALAP_IMPROVE: ALAP IMPROVED scheduling Not Implemented\n");
 	return processors_available;
 }
